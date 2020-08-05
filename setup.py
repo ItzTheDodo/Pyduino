a68d6a3214d5fb452e8b8e53cb013ee7205734bb
ItzTheDodo <57725407+ItzTheDodo@users.noreply.github.com>
2020-08-05 15:31:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
   name="Pyduino",
-  version="1.0"
+  version="1.0",
   author="ItzTheDodo",
   author_email="-",
   description="A library that enables coding for the Arduino",
